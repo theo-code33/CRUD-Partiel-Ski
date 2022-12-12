6a99982d6fa867531df32ef47632738b61a00715
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-12 11:46:56 +0100

    [ADD] Create Post router

---
+++ api/src/routers/post.router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const PostController = require('../controllers/post.controller')
+const router = express.Router()
+
+const endPoint = '/posts'
+
+router.get(`${endPoint}`, PostController.getAll)
+router.get(`${endPoint}/:id`, PostController.getById)
+router.post(`${endPoint}`, PostController.create)
+router.put(`${endPoint}/:id`, PostController.update)
+router.delete(`${endPoint}/:id`, PostController.delete)
+
+module.exports = router
