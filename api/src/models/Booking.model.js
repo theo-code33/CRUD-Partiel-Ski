365851beaecb59e25076547663cd24c8bd66684f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-12 09:29:51 +0100

    Create Booking.model.js
    [ADD] Create Booking model

---
+++ api/src/models/Booking.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bookingSchema = new Schema({
+    telephoneNumber: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    }
+})
+
+const Booking = mongoose.model('Booking', bookingSchema)
+
+module.exports = Booking
