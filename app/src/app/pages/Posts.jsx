61a9a7f2c81012f5569733b6e8b7a954a6f0b4fa
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-12 23:09:30 +0100

    [ADD] Add fetchPost function to fetch posts

--- app/src/app/pages/Posts.jsx
+++ app/src/app/pages/Posts.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react"
+import postService from "../../setup/services/post.service"
+
 const Posts = () => {
+    const [posts, setPosts] = useState([])
+    const fetchPost = async () => {
+        try {
+          const response = await postService.getAll()
+          setPosts(response)
+        } catch (error) {
+          console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchPost()
+    },[])
     return ( 
         <section>
             <h1>Page Posts</h1>
