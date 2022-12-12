1cc455fb3d91ac8c8814b4c63d5ee1cfb44fe3b6
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-12 10:21:50 +0100

    [ADD] Add express.json() middleware

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ const connect = require('./config/mongoose.config')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
 connect()
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 const PORT = process.env.PORT || 8080
 
