df05ced66c07bc13e8c2e6e69ba4fe084c7ecc2e
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-12 23:55:35 +0100

    [ADD] Add fetchPost function to fetch post by id

--- app/src/app/pages/PostPage.jsx
+++ app/src/app/pages/PostPage.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import postService from "../../setup/services/post.service";
 
 const PostPage = () => {
+    const [post, setPost] = useState([])
     const { id } = useParams()
+    const fetchPost = async () => {
+        try {
+            const response = await postService.getOneById(id)
+            setPost(response)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchPost()
+    }, [])
     return ( 
         <div>
             <h1>Post with id {id}</h1>
