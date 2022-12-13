8ba8ba321fc87a09bf2b10432f8c7d7d78a2d26f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-13 13:08:57 +0100

    [UPDATE] Add connection to back on submit booking

--- app/src/app/components/booking/BookingForm.jsx
+++ app/src/app/components/booking/BookingForm.jsx
@@ -1,7 +1,35 @@
-const BookingForm = () => {
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import bookingsService from "../../../setup/services/booking.service";
+
+const BookingForm = ({postId}) => {
+    const [credentials, setCredentials] = useState({
+        post: postId,
+        telephoneNumber: ''
+    })
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setCredentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await bookingsService.create(credentials)
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return ( 
         <div>
-            booking Form
+            <form onSubmit={handleSubmit}>
+                <TextField id="telephoneNumber" name="telephoneNumber" label="Numéro de téléphone" variant="outlined" onInput={handleChange}/>
+                <Button type="submit" variant="contained">Réserver</Button>
+            </form>
         </div>
      );
 }
--- app/src/setup/services/booking.service.js
+++ app/src/setup/services/booking.service.js
@@ -3,7 +3,7 @@ import instance from "./api.service";
 const END_POINT = "/bookings"
 
 const create = async (credentials) => {
-    const response = await instance.post(END_POINT,credentials)
+    const response = await instance.post(END_POINT, credentials)
     return response.data
 }
 
