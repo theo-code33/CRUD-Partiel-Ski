b8cc83652c687e1783cb2cf43af4558a30833421
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-13 16:23:05 +0100

    [UPDATE] Add className

--- app/src/app/components/post/PostItem.jsx
+++ app/src/app/components/post/PostItem.jsx
@@ -3,7 +3,7 @@ import PostMainContent from "./PostMainContent";
 
 const PostItem = ({post}) => {
     return ( 
-        <div>
+        <div className="container-post-item">
             <Link to={`/post/${post._id}`}>
                 <div className="image-container">
                     <img src={post.imgUrl ? post.imgUrl : '/skiing-with-a-view-over-the-mountains.jpg'} alt={`Image du post ${post.title}`} />
--- app/src/app/components/post/PostMainContent.jsx
+++ app/src/app/components/post/PostMainContent.jsx
@@ -2,9 +2,9 @@ const PostMainContent = ({post}) => {
     return ( 
         <div className="content-post">
             <h2>{post.title}</h2>
-            <div>
+            <div className="container-info">
                 <span>{post.price}€ / j</span>
-                <span>{post.size} cm</span>
+                <span className="size">{post.size} cm</span>
             </div>
             <p>{post.address}</p>
         </div>
