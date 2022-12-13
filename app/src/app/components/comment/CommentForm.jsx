dfe5c1d4a0fc29f7b4cca321acf5d12f7f22f2fe
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-13 14:19:04 +0100

    [UPDATE] Create comment form
    Create comment form and connect it to API

--- app/src/app/components/comment/CommentForm.jsx
+++ app/src/app/components/comment/CommentForm.jsx
@@ -1,7 +1,46 @@
-const CommentForm = () => {
+import { Button, Rating, TextField } from "@mui/material";
+import { useState } from "react";
+import commentsService from "../../../setup/services/comment.service";
+
+const CommentForm = ({postId}) => {
+    const [credentials, setCredentials] = useState({
+        post: postId,
+        username: '',
+        description: '',
+        stars: 0
+    })
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setCredentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await commentsService.create(credentials)
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return ( 
         <div>
-            Comment Form
+            <form onSubmit={handleSubmit}>
+                <Rating name="stars" id="stars" onInput={handleChange} />
+                <TextField name="username" id="username" variant="outlined" label="Votre Nom" onInput={handleChange} />
+                <TextField 
+                    name="description"
+                    id="description"
+                    multiline 
+                    rows={6} 
+                    variant="outlined"
+                    label="Votre commentaire"
+                    onInput={handleChange} 
+                />
+                <Button type="submit" variant="contained">Ajouter un commentaire</Button>
+            </form>
         </div>
      );
 }
