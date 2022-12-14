3be86088b8b05c2ccd80513daefa11b6b4b5ccfb
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-14 18:58:34 +0100

    [ADD] Create shop service

---
+++ app/src/setup/services/shop.service.js
@@ -0,0 +1,26 @@
+import instance from "./api.service";
+
+const END_POINT = "/shops"
+
+const getOneById = async (id) => {
+    const response = await instance.get(`${END_POINT}/${id}`)
+    return response.data
+}
+
+const getOne = async (credentials) => {
+    const response = await instance.post(`${END_POINT}/connect`, credentials)
+    return response.data
+}
+
+const update = async (id, credentials) => {
+    const response = await instance.put(`${END_POINT}/${id}`, credentials)
+    return response.data
+}
+
+const shopService = {
+    getOneById,
+    getOne,
+    update
+}
+
+export default shopService
