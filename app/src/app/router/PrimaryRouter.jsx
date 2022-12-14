44f338a993b11865694362000e1fdd4d32029a60
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-14 18:58:14 +0100

    [UPDATE] Add Route for connexion and shop page

--- app/src/app/router/PrimaryRouter.jsx
+++ app/src/app/router/PrimaryRouter.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import PostPage from "../pages/PostPage";
-import PostsListingPage from "../pages/PostsListingPage";
+import ConnectPage from "../pages/admin/ConnectPage";
+import ShopPage from "../pages/admin/ShopPage";
+import PostPage from "../pages/client/PostPage";
+import PostsListingPage from "../pages/client/PostsListingPage";
 
 const PrimaryRouter = () => {
-    return ( 
+    const [isConnected, setIsConnected] = useState('')
+    useEffect(() => {
+        const isConnectedLocalStorage = window.localStorage.getItem('isConnected')
+        setIsConnected(isConnectedLocalStorage)
+    }, [])
+    return (
         <Routes>
             <Route path="/" element={<PostsListingPage />}/>
             <Route path="/post/:id" element={<PostPage />}/>
+            <Route path="/shop/connect" element={<ConnectPage />} />
+            {isConnected === 'true'
+                ? <Route path="/shop/:id" element={<ShopPage />} />
+                : <Route path="/shop/*" element={<ConnectPage />} />
+            }
         </Routes>
      );
 }
