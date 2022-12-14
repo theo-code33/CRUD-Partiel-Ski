43ada418d82052350c5ac5905b3d8ce1dcb0e691
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-14 11:50:55 +0100

    [ADD] Create Shop Model

---
+++ api/src/models/Shop.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const shopSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: [6, 'Password must be at least 6 character long']
+    },
+    logoUrl: {
+        type: String
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Shop = mongoose.model('Shop', shopSchema)
+
+module.exports = Shop
