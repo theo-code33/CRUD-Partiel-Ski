2fb225fc99eeb601fb1a522eb42397865ac82a6a
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-14 14:38:36 +0100

    [UPDATE] Add routes to get post by shop

--- api/src/routers/post.router.js
+++ api/src/routers/post.router.js
@@ -6,6 +6,7 @@ const endPoint = '/posts'
 
 router.get(`${endPoint}`, PostController.getAll)
 router.get(`${endPoint}/:id`, PostController.getById)
+router.get(`/:id${endPoint}`, PostController.getByShop)
 router.post(`${endPoint}`, PostController.create)
 router.put(`${endPoint}/:id`, PostController.update)
 router.delete(`${endPoint}/:id`, PostController.delete)
