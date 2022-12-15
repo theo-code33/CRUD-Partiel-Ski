5d66e2df533863caf0d0c2326f35185456b6eb81
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 15:49:59 +0100

    [UPDATE] Add button to access to Shop page
    Add button when user is connected to access to shop page

--- app/src/app/layout/MainLayout.jsx
+++ app/src/app/layout/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material";
 import FilterForm from "../components/FilterForm";
 
-const MainLayout = ({children, setPostsFiltered, posts}) => {
+const MainLayout = ({children, setPostsFiltered, posts, shopID}) => {
     return ( 
         <>
         <h1 className="title-app">ThomasAuSki.com</h1>
-        <Button variant="contained" href="/shop/connect" sx={{position: "absolute", right: "50px", top: "70px"}}>Se connecter</Button>
+        {shopID
+        ? <Button variant="contained" href={`/shop/${shopID}`} sx={{position: "absolute", right: "50px", top: "70px"}}>Ma Boutique</Button>
+        : <Button variant="contained" href="/shop/connect" sx={{position: "absolute", right: "50px", top: "70px"}}>Se connecter</Button>
+        }
         <FilterForm setPostsFiltered={setPostsFiltered} posts={posts}/>
         {children}
         </>
