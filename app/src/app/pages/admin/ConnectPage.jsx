877bdcee77c0dbad467bdf606b9a7baa6934a326
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 10:10:05 +0100

    [UPDATE] Add Shop Layout to connect page

--- app/src/app/pages/admin/ConnectPage.jsx
+++ app/src/app/pages/admin/ConnectPage.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import shopService from "../../../setup/services/shop.service";
 import ConnectFormShop from "../../components/shop/ConnectFormShop";
+import ShopLayout from "../../layout/ShopLayout";
 
-const ConnectPage = () => {
-    return ( 
-        <>
-            <ConnectFormShop/>
-        </>
+const ConnectPage = ({setShopID}) => {
+    return (
+        <ShopLayout>
+            <section className="connect-page">
+                <h1>Se connecter</h1>
+                <ConnectFormShop setShopID={setShopID}/>
+            </section>
+        </ShopLayout>
      );
 }
  
