d5c14b1b5bfe4a564e76af297b887aed698d87cd
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 22:39:38 +0100

    [UPDATE] Add route to getBookingByShop method

--- api/src/routers/shop.router.js
+++ api/src/routers/shop.router.js
@@ -7,6 +7,7 @@ const endPoint = '/shops'
 router.get(`${endPoint}`, ShopController.getAll)
 router.post(`${endPoint}/connect`, ShopController.getOne)
 router.get(`${endPoint}/:id`, ShopController.getById)
+router.get(`${endPoint}/:id/bookings`, ShopController.getBookingsByShop)
 router.post(`${endPoint}`, ShopController.create)
 router.put(`${endPoint}/:id`, ShopController.update)
 router.delete(`${endPoint}/:id`, ShopController.delete)
