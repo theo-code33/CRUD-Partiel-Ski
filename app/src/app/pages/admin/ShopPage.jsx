083908aa61847d662fab7d5c339188bb4300e998
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 13:42:26 +0100

    [UPDATE] Add ShopLayout, HeaderShop and ShopListing component

--- app/src/app/pages/admin/ShopPage.jsx
+++ app/src/app/pages/admin/ShopPage.jsx
@@ -1,9 +1,33 @@
-const ShopPage = ({isConnected}) => {
+import { useEffect, useState } from "react";
+import shopService from "../../../setup/services/shop.service";
+import HeaderShop from "../../components/shop/HeaderShop";
+import ShopPostListing from "../../components/shop/ShopPostListing";
+import ShopLayout from "../../layout/ShopLayout";
+
+const ShopPage = ({shopID}) => {
+    const [shop, setShop] = useState(null)
+    const fetchShopData = async (shopID) => {
+        try {
+            const shopResponse = await shopService.getOneById(shopID)
+            setShop(shopResponse)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchShopData(shopID)
+    },[])
     return ( 
-        <div>
-            Hello Shop
-        </div>
-     );
+        <ShopLayout>
+            {shop &&
+                <>
+                    <HeaderShop shop={shop}/>
+                    <ShopPostListing shopPosts={shop.posts} fetchShopData={fetchShopData} shopID={shopID}/>
+                </>
+            }
+        </ShopLayout>    
+    );
 }
  
 export default ShopPage;
