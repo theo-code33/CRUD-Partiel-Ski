56701e616f52cd673c9bad836c777efe5ae12b49
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 13:34:21 +0100

    [UPDATE] Add many route for connected user
    Add Route for Create Post, Update Post and parameters page of Shop

--- app/src/app/router/PrimaryRouter.jsx
+++ app/src/app/router/PrimaryRouter.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ConnectPage from "../pages/admin/ConnectPage";
+import CreatePostPage from "../pages/admin/CreatePostPage";
+import ParameterPage from "../pages/admin/ParameterPage";
 import ShopPage from "../pages/admin/ShopPage";
+import UpdatePost from "../pages/admin/UpdatePostPage";
 import PostPage from "../pages/client/PostPage";
 import PostsListingPage from "../pages/client/PostsListingPage";
 
@@ -18,9 +21,14 @@ const PrimaryRouter = () => {
             <Route path="/post/:id" element={<PostPage />}/>
             <Route path="/shop/connect" element={<ConnectPage setShopID={setShopID} />} />
             <Route path="/shop/*" element={<ConnectPage setShopID={setShopID} />} />
-            {shopID != null
-                ? <Route path={`/shop/${shopID}`} element={<ShopPage />} />
-                : null
+            {shopID != null && (
+                <>
+                    <Route path={`/shop/${shopID}`} element={<ShopPage shopID={shopID} />} />
+                    <Route path={`/shop/${shopID}/parametres`} element={<ParameterPage shopID={shopID} />} />
+                    <Route path='/post/update/:id' element={<UpdatePost />}/>
+                    <Route path='/post/create' element={<CreatePostPage shopID={shopID} />}/>
+                </>
+            )
             }
         </Routes>
      );
