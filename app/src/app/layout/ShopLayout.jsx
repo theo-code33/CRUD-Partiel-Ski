9d775e4972a7bab564c5a66516e8451870273f3e
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 10:08:13 +0100

    [ADD] Create Shop Layout

---
+++ app/src/app/layout/ShopLayout.jsx
@@ -0,0 +1,10 @@
+const ShopLayout = ({children}) => {
+    return (
+        <>
+            <h1 className="title-app">ThomasAuSki.com</h1>
+            {children}
+        </>
+     );
+}
+ 
+export default ShopLayout;
