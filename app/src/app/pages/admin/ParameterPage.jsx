bb0142b9caa46924d674bb59191cc5bab06a0239
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 22:43:10 +0100

    [UPDATE] Add UpdateShopForm component

--- app/src/app/pages/admin/ParameterPage.jsx
+++ app/src/app/pages/admin/ParameterPage.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import shopService from "../../../setup/services/shop.service";
+import UpdateShopForm from "../../components/shop/UpdateShopForm";
+
 const ParameterPage = ({shopID}) => {
+    const [shop, setShop] = useState(null)
+    const fetchShopData = async (id) => {
+        try {
+            const shopResponse = await shopService.getOneById(id)
+            setShop(shopResponse)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchShopData(shopID)
+    },[])
+
     return ( 
         <div>
-            Shop Parameter
+            {shop &&
+                <UpdateShopForm shop={shop}/>
+            }
         </div>
      );
 }
