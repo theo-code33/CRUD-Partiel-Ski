e2d4daa17d99076b1929be936a762d9d1348deb2
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-15 10:10:56 +0100

    [UPDATE] Add Button to redirect to connect page

--- app/src/app/layout/MainLayout.jsx
+++ app/src/app/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import FilterForm from "../components/FilterForm";
 
 const MainLayout = ({children, setPostsFiltered, posts}) => {
     return ( 
         <>
         <h1 className="title-app">ThomasAuSki.com</h1>
+        <Button variant="contained" href="/shop/connect" sx={{position: "absolute", right: "50px", top: "70px"}}>Se connecter</Button>
         <FilterForm setPostsFiltered={setPostsFiltered} posts={posts}/>
         {children}
         </>
