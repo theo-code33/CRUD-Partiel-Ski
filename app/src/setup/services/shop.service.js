adee10ad69ba250064191b82f39366b58750fda4
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-16 02:10:03 +0100

    [UPDATE] Add create and delete method in Shop.service

--- app/src/setup/services/shop.service.js
+++ app/src/setup/services/shop.service.js
@@ -17,8 +17,18 @@ const getBookings = async (id) => {
     return response.data
 }
 
+const create = async (credentials) => {
+    const response = await instance.post(END_POINT, credentials)
+    return response.data
+}
+
 const update = async (id, credentials) => {
     const response = await instance.put(`${END_POINT}/${id}`, credentials)
+    return response.data
+}
+
+const remove = async (id) => {
+    const response = await instance.delete(`${END_POINT}/${id}`)
     return response.data
 }
 
@@ -26,7 +36,9 @@ const shopService = {
     getOneById,
     getOne,
     getBookings,
-    update
+    create,
+    update,
+    remove
 }
 
 export default shopService
