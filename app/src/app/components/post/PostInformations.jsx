7ca2949317af87376c1b25cd06ade1c0718a039d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-16 02:15:07 +0100

    [UPDATE] Add Favorite actions

--- app/src/app/components/post/PostInformations.jsx
+++ app/src/app/components/post/PostInformations.jsx
@@ -1,7 +1,45 @@
 import { Rating } from "@mui/material";
 import PostMainContent from "./PostMainContent";
+import { IconButton } from "@mui/material";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useEffect, useState } from "react";
 
 const PostInformations = ({post, ratingPost}) => {
+
+    const [isFavorite, setIsFavorite] = useState(false)
+
+    useEffect(() => {
+        const favorites = JSON.parse(window.localStorage.getItem('favorites'))
+        if (favorites) {
+            const isFavorite = favorites.find(favorite => favorite._id === post._id)
+            if (isFavorite) {
+                setIsFavorite(true)
+            }
+        }
+    }, [])
+
+    const handleFavorite = () => {
+        const favorites = JSON.parse(window.localStorage.getItem('favorites'))
+        if(!isFavorite){
+            if (favorites) {
+                const postFavorite = window.localStorage.setItem('favorites', JSON.stringify([...favorites, post]))
+            }else{
+                const postFavorite = window.localStorage.setItem('favorites', JSON.stringify([post]))
+            }
+        }else{
+            if (favorites) {
+                const postFavorite = window.localStorage.setItem('favorites', JSON.stringify([...favorites.filter(favorite => favorite._id !== post._id)]))
+            }else{
+                const postFavorite = window.localStorage.setItem('favorites', JSON.stringify([]))
+            }
+        }
+        setIsFavorite((isFavorite) => !isFavorite);
+    }
+
+    const handleMouseDownFavorite = (event) => {
+        event.preventDefault();
+    };
 
     return ( 
         <div className="container-content-post">
@@ -9,7 +47,17 @@ const PostInformations = ({post, ratingPost}) => {
                 <img src={post.imgUrl ? post.imgUrl : '/skiing-with-a-view-over-the-mountains.jpg'} alt={`Image du post ${post.title}`} />
             </div>
             <div className="post-content">
-                <Rating name="rating" value={ratingPost} readOnly />
+                <div className="post-content-top">
+                    <Rating name="rating" value={ratingPost} readOnly />
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleFavorite}
+                        onMouseDown={handleMouseDownFavorite}
+                        edge="end"
+                        >
+                        {isFavorite ? <FavoriteIcon color="error" fontSize="large"/> : <FavoriteBorderIcon fontSize="large"/>}
+                    </IconButton>
+                </div>
                 <PostMainContent post={post}/>
                 <p>{post.description}</p>
             </div>
