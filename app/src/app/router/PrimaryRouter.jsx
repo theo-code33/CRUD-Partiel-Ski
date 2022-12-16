6ca2eaee9025f014558542216e189beec4b63bca
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2022-12-16 02:11:33 +0100

    [UPDATE] Add post favorite and shop create route
    Add route to post favorite page ( /posts/favorites )
    Add route to create shop page ( /shop/create )

--- app/src/app/router/PrimaryRouter.jsx
+++ app/src/app/router/PrimaryRouter.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import BookingsPage from "../pages/admin/BookingsPage";
 import ConnectPage from "../pages/admin/ConnectPage";
 import CreatePostPage from "../pages/admin/CreatePostPage";
+import CreateShopPage from "../pages/admin/CreateShopPage";
 import ParameterPage from "../pages/admin/ParameterPage";
 import ShopPage from "../pages/admin/ShopPage";
 import UpdatePost from "../pages/admin/UpdatePostPage";
+import FavoritePage from "../pages/client/FavoritePage";
 import PostPage from "../pages/client/PostPage";
 import PostsListingPage from "../pages/client/PostsListingPage";
 
@@ -20,7 +22,9 @@ const PrimaryRouter = () => {
             <Route path="/" element={<PostsListingPage shopID={shopID}/>}/>
             <Route path="/*" element={<PostsListingPage shopID={shopID} />}/>
             <Route path="/post/:id" element={<PostPage />}/>
+            <Route path="/posts/favorites" element={<FavoritePage />}/>
             <Route path="/shop/connect" element={<ConnectPage setShopID={setShopID} />} />
+            <Route path="/shop/create" element={<CreateShopPage />} />
             <Route path="/shop/*" element={<ConnectPage setShopID={setShopID} />} />
             {shopID != null && (
                 <>
